ceea01ec2a48a87572cce769ddbd86b15520152f
Kavinda Senarathne <kavindasenarathne94@gmail.com>
2018-09-16 18:31:42 +0530

    Content panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HistoryActivity';
+  body="This is body";
 }
--- src/app/contents/contents.component.ts
+++ src/app/contents/contents.component.ts
@@ -12,7 +12,7 @@ export class ContentsComponent implements OnInit {
   constructor() { }
 
 
-  
+
   ngOnInit() {
   }
 
